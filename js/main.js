8713f7edd7872eedb33cb912fe17a2a67d3919ab
deivan <ariastation@ua.fm>
2015-12-03 23:21:37 +0200

    Added links structure and some info for it

--- js/main.js
+++ js/main.js
@@ -16,13 +16,13 @@ var RenameDrive = {
         lake:       "затока",
         island:     "острів",
         station:    "станція"
-        
     },
+    linkTypes: ["особа", "подія", "об’єкт", "суб’єкт"],
     data: {},
       
     render: function () {
         var self = this, arr, 
-            core = $("#rename-data"), head, data, strings = "", item;
+            core = $("#rename-data"), head, data, strings = "", item, link;
     
         $.ajax({
             method: "GET",
@@ -39,13 +39,17 @@ var RenameDrive = {
                 head = $("<h3>"+ arr[key].newAreaName + " (" +arr[key].oldAreaName+ ")" +"</h3>");
                 strings = "";
                 if(arr[key].objects.length > 0) {
-                    strings += '<div class="data-line-header"><div class="object-type">&nbsp;</div><div class="object-old">Стара назва</div><div class="object-new">Нова назва</div></div>';
+                    strings += '<div class="data-line-header"><div class="object-type">&nbsp;</div><div class="object-old">Стара назва</div><div class="object-new">Нова назва</div><div class="object-link">На честь</div></div>';
                     for (var i=0, l = arr[key].objects.length; i<l ; i++) {
                         item = arr[key].objects[i];
+                        link = item.link === undefined ? 
+                               "" : 
+                               '<a href="'+item.link.href+'">'+self.linkTypes[item.link.type]+'</a>';
                         strings += '<div class="data-line"><div class="object-type">' + 
                                 self.types[item.type] + 
-                                '</div><div class="object-old">'+ item.oldName + 
-                                '</div><div class="object-new">'+ item.newName + 
+                                '</div><div class="object-old">'+  item.oldName + 
+                                '</div><div class="object-new">'+  item.newName + 
+                                '</div><div class="object-link">'+ link +
                                 "</div></div>";
                     }
                 }
