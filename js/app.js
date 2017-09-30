be7b8f2cfe47b6f2ef655fa7a515ce1212d0bef4
deivan <ariastation@ua.fm>
2017-10-01 00:06:05 +0300

    added booklet

--- js/app.js
+++ js/app.js
@@ -124,6 +124,8 @@ class Renames {
           <p>Запрошуємо завантажити додаток для ОС Android: \
           <a href="https://play.google.com/store/apps/details?id=ua.dp.rename.dniprostreets&amp;utm_source=global_co&amp;utm_medium=prtnr&amp;utm_content=Mar2515&amp;utm_campaign=PartBadge&amp;pcampaignid=MKT-AC-global-none-all-co-pr-py-PartBadges-Oct1515-1"><img alt="Get it on Google Play" src="https://play.google.com/intl/en_us/badges/images/apps/ru-play-badge.png" width="82" height="25"></a></p>\n\
           <p>Також для пошуку <a href="http://telegram.me/renamedpuabot">можливо скористатись Телеграм-ботом</a></p>\
+          <p>Позначкою  <img src="img/restore.png" />  помічена повернута історична назва об\'єкту.</p>\
+          <p class="special">Завантажте авторський буклет з переліком перейменувань від <a href="http://atomoxod.in.ua">Олексія Пильова</a>, у вигляді файла формату PDF <a href="Dnipro_Rename_v2.pdf">за цим посиланням</a></p>\
           <h3>Пошук за назвою (бажано українською):</h3>\
           <input class="input-search" v-model.trim="search" @keyup="filter" />\
             <table class="table-view" v-for="tab in filteredData" v-if="tab.obj.length > 0">\
