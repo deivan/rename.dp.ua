5c2fb9ff9e44b36bed483a017ec7294e9b46013c
Ivan <idemenkov@archer-soft.com>
2019-10-01 19:01:25 +0300

    added new app and updated DB

---
+++ src/views/About.vue
@@ -0,0 +1,28 @@
+<template>
+  <div class="content">
+    <h1>Про проект</h1>
+    <article>
+      <h4>Як все починалось та навіщо все це</h4>
+      <p>Детальна стаття про проект на DOU.ua - <a href="https://dou.ua/lenta/articles/dou-projector-rename/">Rename.dp.ua — справочник о переименовании улиц Днепра, написанный за 2 дня</a></p>
+      <p>Сюжет 51-го телеканалу м. Дніпра у жовтні 2016, автор - <a href="https://www.facebook.com/profile.php?id=100007976653247">Тетяна Консевич</a> </p>
+      <iframe width="560" height="315" src="https://www.youtube.com/embed/oSsYiAqoS-Y" frameborder="0" allowfullscreen=""></iframe>
+      <h4>Хто ми?</h4><p>Автор та керманич проекту - <a href="http://demenkov.dp.ua/">Іван Юрійович ДЕМЕНКОВ</a>
+      <p>Авторська графіка для шапки сайту та буклет - <a href="http://atomoxod.in.ua">Олексій Пильов</a>.</p>
+      <p>Наповнення бази даних, пошук актуальних посиланнь та корекція помилок -  <a href="https://facebook.com/rasim.labibov">Расім Лабібов</a></p>
+      <p>Перший <a href="https://play.google.com/store/apps/details?id=ua.dp.rename.dniprostreets">мобільний додаток для ОС Андроид</a> - зробив <a href="https://www.facebook.com/den.drobiazko">Den Drobiazko</a></p>
+      <p>Розробник <a href="http://telegram.me/renamedpuabot">телеграм-боту</a> для пошуку нових назв - <a href="https://www.facebook.com/stanislav.blokh">Stanislav Blokh</a></p>
+      <h4>Чи потрібна проекту допомога?</h4><p>Цій сервіс створений дніпрянами для дніпрян. Все бескоштовно. Якщо ви маєте сили та бажання покращити проект - приєднуйтесь до нас <a href="https://github.com/deivan/rename.dp.ua">у мережі проектів GitHub</a></p>
+      <p>Малюнки та побажання ласкаво просимо слати за адресою: <span style="color:#00a; text-decoration:underline">info@rename.dp.ua</span></p>
+    </article>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'AboutPage'
+}
+</script>
+
+<style scoped>
+
+</style>
