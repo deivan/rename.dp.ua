6d0a61ea24187d9c6b6bb2f7b7cfd62308ae7265
Ivan Demenkov <ariastation@ua.fm>
2022-04-25 16:20:17 +0300

    updated JSON file, added update for 2020

--- public/js/data.js
+++ public/js/data.js
@@ -1520,6 +1520,15 @@ const DATA = `
                 "type": "street",
                 "oldName": "Білгородська",
                 "newName": "Наукових працівників"
+            },
+            {
+                "type": "street",
+                "oldName": "Медична",
+                "newName": "Академіка Георгія Дзяка",
+                "link": {
+                    "href": "https://uk.wikipedia.org/wiki/%D0%94%D0%B7%D1%8F%D0%BA_%D0%93%D0%B5%D0%BE%D1%80%D0%B3%D1%96%D0%B9_%D0%92%D1%96%D0%BA%D1%82%D0%BE%D1%80%D0%BE%D0%B2%D0%B8%D1%87",
+                    "type": 0
+                }
             }
         ]
     },
